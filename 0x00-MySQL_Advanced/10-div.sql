cf751c1b9443b8896859de39b6d1b49847e65680
Ahercode <philipkelly407@gmail.com>
2023-01-12 18:03:37 +0000

    Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,5 +1,6 @@
 -- script that creates a function SafeDiv that divides (and returns) the
 -- first by the second number or returns 0 if the second number is equal to 0
+-- by phils
 
 DROP FUNCTION IF EXISTS SafeDiv;
 
