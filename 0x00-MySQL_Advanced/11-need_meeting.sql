47f865e91c5907893cce77a4402f18a50631a577
Ahercode <philipkelly407@gmail.com>
2023-01-12 18:04:42 +0000

    No table for a meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,6 @@
 -- script that creates a view need_meeting that lists all students that
 -- have a score under 80 (strict) and no last_meeting or more than 1 month
+-- by phils 
 
 DROP VIEW IF EXISTS need_meeting;
 
